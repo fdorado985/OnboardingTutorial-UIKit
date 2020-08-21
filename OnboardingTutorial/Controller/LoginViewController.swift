b149774d2c3c5f631e0a590d9deb8858b53613f7
Juan Dorado <fdorado985@gmail.com>
2020-08-21 01:11:14 -0500

    Add textfields for login

--- OnboardingTutorial/Controller/LoginViewController.swift
+++ OnboardingTutorial/Controller/LoginViewController.swift
@@ -14,6 +14,47 @@ class LoginViewController: UIViewController {
 
   private let iconImageView = UIImageView(image: #imageLiteral(resourceName: "firebase-logo"))
 
+  private let emailTextField: UITextField = {
+    let textField = UITextField()
+
+    let spacer = UIView()
+    spacer.setDimensions(height: 50, width: 12)
+    textField.leftView = spacer
+    textField.leftViewMode = .always
+
+    textField.borderStyle = .none
+    textField.textColor = .white
+    textField.keyboardAppearance = .dark
+    textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
+    textField.setHeight(height: 50)
+    textField.attributedPlaceholder = NSAttributedString(
+      string: "Email",
+      attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)]
+    )
+    return textField
+  }()
+
+  private let passwordTextField: UITextField = {
+    let textField = UITextField()
+
+    let spacer = UIView()
+    spacer.setDimensions(height: 50, width: 12)
+    textField.leftView = spacer
+    textField.leftViewMode = .always
+
+    textField.borderStyle = .none
+    textField.textColor = .white
+    textField.keyboardAppearance = .dark
+    textField.isSecureTextEntry = true
+    textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
+    textField.setHeight(height: 50)
+    textField.attributedPlaceholder = NSAttributedString(
+      string: "Password",
+      attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)]
+    )
+    return textField
+  }()
+
   // MARK: - View lifecycle
 
   override func viewDidLoad() {
@@ -29,6 +70,7 @@ extension LoginViewController {
     navigationController?.navigationBar.barStyle = .black
     configureBackground()
     configureIconImageView()
+    configureLoginFields()
   }
 
   private func configureBackground() {
@@ -44,5 +86,22 @@ extension LoginViewController {
     iconImageView.centerX(inView: view)
     iconImageView.setDimensions(height: 120, width: 120)
     iconImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+  }
+
+  private func configureLoginFields() {
+    let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+    stackView.axis = .vertical
+    stackView.spacing = 20
+    stackView.distribution = .fillEqually
+
+    view.addSubview(stackView)
+    stackView.anchor(
+      top: iconImageView.bottomAnchor,
+      left: view.leftAnchor,
+      right: view.rightAnchor,
+      paddingTop: 32,
+      paddingLeft: 32,
+      paddingRight: 32
+    )
   }
 }
