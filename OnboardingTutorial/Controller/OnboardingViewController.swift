12aeae4d5b585c3a89a5e495dc2fd76c1a81aa3f
Juan Dorado <fdorado985@gmail.com>
2020-08-23 15:20:49 -0500

    Finish UI Onboarding adding a get started button that appears just on the last onboarding item

--- OnboardingTutorial/Controller/OnboardingViewController.swift
+++ OnboardingTutorial/Controller/OnboardingViewController.swift
@@ -15,6 +15,18 @@ class OnboardingViewController: UIViewController {
 
   private var onboardingItems = [OnboardingItemInfo]()
   private var onboardingView = PaperOnboarding()
+  private let getStartedButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("Get Started", for: .normal)
+    button.setTitleColor(.white, for: .normal)
+    button.titleLabel?.font = .boldSystemFont(ofSize: 24)
+    button.addTarget(self, action: #selector(getStartedButtonDidTap), for: .touchUpInside)
+    return button
+  }()
+
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
 
   // MARK: - View Lifecycle
 
@@ -23,7 +35,19 @@ class OnboardingViewController: UIViewController {
     configureViewController()
   }
 
+  // MARK: - Actions
+
+  @objc private func getStartedButtonDidTap(_ sender: UIButton) {
+
+  }
+
   // MARK: - Methods
+
+  private func animateGetStartedButton(_ shouldShow: Bool) {
+    UIView.animate(withDuration: 0.5) {
+      self.getStartedButton.alpha = shouldShow ? 1 : 0
+    }
+  }
 }
 
 // MARK: - PaperOnboardingDataSource
@@ -39,6 +63,16 @@ extension OnboardingViewController: PaperOnboardingDataSource {
   }
 }
 
+// MARK: -
+
+extension OnboardingViewController: PaperOnboardingDelegate {
+
+  func onboardingWillTransitonToIndex(_ index: Int) {
+    let shouldShowGetStartedButton = index == onboardingItems.count - 1
+    animateGetStartedButton(shouldShowGetStartedButton)
+  }
+}
+
 // MARK: - Setup View
 
 extension OnboardingViewController {
@@ -49,6 +83,7 @@ extension OnboardingViewController {
 
   private func setupLayouts() {
     configureOnboardingView()
+    configureGetStartedButton()
   }
 
   private func configureOnboardingView() {
@@ -60,17 +95,40 @@ extension OnboardingViewController {
   private func configureOnboardingDataSource() {
     let item1 = OnboardingItemInfo(
       informationImage: #imageLiteral(resourceName: "baseline_insert_chart_white_48pt").withRenderingMode(.alwaysOriginal),
-      title: "Metrics", description: "Some description here...",
+      title: OT_ONBOARDINGVC_METRICS_TITLE, description: OT_ONBOARDINGVC_METRICS_DESCRIPTION,
       pageIcon: UIImage(), color: .systemPurple,
       titleColor: .white, descriptionColor: .white,
       titleFont: .boldSystemFont(ofSize: 24), descriptionFont: .systemFont(ofSize: 16)
     )
 
+    let item2 = OnboardingItemInfo(
+      informationImage: #imageLiteral(resourceName: "baseline_dashboard_white_48pt").withRenderingMode(.alwaysOriginal),
+      title: OT_ONBOARDINGVC_DASHBOARD_TITLE, description: OT_ONBOARDINGVC_DASHBOARD_DESCRIPTION,
+      pageIcon: UIImage(), color: .systemBlue,
+      titleColor: .white, descriptionColor: .white,
+      titleFont: .boldSystemFont(ofSize: 24), descriptionFont: .systemFont(ofSize: 16)
+    )
+
+    let item3 = OnboardingItemInfo(
+      informationImage: #imageLiteral(resourceName: "baseline_notifications_active_white_48pt").withRenderingMode(.alwaysOriginal),
+      title: OT_ONBOARDINGVC_NOTIFICATIONS_TITLE, description: OT_ONBOARDINGVC_NOTIFICATIONS_DESCRIPTION,
+      pageIcon: UIImage(), color: .systemPink,
+      titleColor: .white, descriptionColor: .white,
+      titleFont: .boldSystemFont(ofSize: 24), descriptionFont: .systemFont(ofSize: 16)
+    )
+
     onboardingItems.append(item1)
-    onboardingItems.append(item1)
-    onboardingItems.append(item1)
+    onboardingItems.append(item2)
+    onboardingItems.append(item3)
 
     onboardingView.dataSource = self
+    onboardingView.delegate = self
     onboardingView.reloadInputViews()
+  }
+
+  private func configureGetStartedButton() {
+    view.addSubview(getStartedButton)
+    getStartedButton.centerX(inView: view)
+    getStartedButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingBottom: 128)
   }
 }
---
+++ OnboardingTutorial/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  OnboardingTutorial
+//
+//  Created by Juan Francisco Dorado Torres on 22/08/20.
+//  Copyright © 2020 Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+
+let OT_ONBOARDINGVC_METRICS_TITLE = "Metrics"
+let OT_ONBOARDINGVC_DASHBOARD_TITLE = "Dashboard"
+let OT_ONBOARDINGVC_NOTIFICATIONS_TITLE = "Get Notified"
+
+let OT_ONBOARDINGVC_METRICS_DESCRIPTION = "Extract valuable insights and come up with data driven product initiatives to help grow your business."
+let OT_ONBOARDINGVC_DASHBOARD_DESCRIPTION = "Get notified when important stuff is happening, so you don't miss out on the action."
+let OT_ONBOARDINGVC_NOTIFICATIONS_DESCRIPTION = "Everything you need all in one place, available through our dashboard feature."
